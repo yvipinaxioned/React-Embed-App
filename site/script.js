646bdfb0ebc243cc4ec9f3b206a6ddca2481b212
Vipin Yadav <vipiny35@gmail.com>
2024-08-08 16:26:16 +0530

    open app on click

--- site/script.js
+++ site/script.js
@@ -1,3 +1,4 @@
+var rootElement;
 (function () {
   // Ensure the global configuration variables are set
   const config = window.MY_EMBED_APP_CONFIG || {};
@@ -5,11 +6,18 @@
   /** Ensure the root element exists
    * NOTE: This id should match the id of the root element in the React app index.tsx file
   */
-  let rootElement = document.getElementById('my-embed-app-react-root');
+  rootElement = document.getElementById('my-embed-app-react-root');
   if (!rootElement) {
     rootElement = document.createElement('div');
     rootElement.id = 'my-embed-app-react-root';
     document.body.appendChild(rootElement);
+    
+    // Style the root element to position it in the bottom right corner
+    rootElement.style.display = 'none';
+    rootElement.style.position = 'fixed';
+    rootElement.style.bottom = '20px';
+    rootElement.style.right = '20px';
+    rootElement.style.zIndex = '99999999';
   }
 
   const script = document.createElement('script');
@@ -17,3 +25,7 @@
   rootElement.appendChild(script);
 
 })();
+
+function openApp() {
+  rootElement.style.display = 'block';
+}
